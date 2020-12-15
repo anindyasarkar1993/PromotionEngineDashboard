bcc8dc7ecb92180eab4d4cc99b5135a4123e78cd
Anindya Sarkar <kumarAnindyan@outlook.com>
2020-12-15 10:15:56 +0530

    Product Class Added

--- Promotion_Engine.UnitTest/PromoEngineTest.cs
+++ Promotion_Engine.UnitTest/PromoEngineTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using Promotion_Engine;
 
 namespace Promotion_Engine.UnitTest
 {
@@ -9,6 +11,15 @@ namespace Promotion_Engine.UnitTest
         [Test]
         public void GetTotalPriceBackSingleProductTestA()
         {
+
+            //List<Product> lp = new List<Product>()
+            //{
+            //    new Product(){Id = "A", Price = 50},
+            //    new Product(){Id = "B", Price = 30},
+            //    new Product(){Id = "C", Price = 20}
+            //};
+            //var result = Program.GetTotalPrice(lp);
+            //Assert.That(result, Is.EqualTo(100));
         }
     }
 }
