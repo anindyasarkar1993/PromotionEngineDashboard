256e904aa53d2c13558e28c57bd12a696e133b21
Anindya Sarkar <kumarAnindyan@outlook.com>
2020-12-15 10:02:22 +0530

    Adding NUnit in Visual Studio for unit Test set up

---
+++ Promotion_Engine.UnitTest/PromoEngineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Promotion_Engine.UnitTest
+{
+    [TestFixture]
+    public class PromoEngineTest
+    {
+        [Test]
+        public void GetTotalPriceBackSingleProductTestA()
+        {
+        }
+    }
+}
---
+++ Promotion_Engine.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Promotion_Engine.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Promotion_Engine.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52ab029e-1eb7-4c1a-9ba1-6bd1d7c91d5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
