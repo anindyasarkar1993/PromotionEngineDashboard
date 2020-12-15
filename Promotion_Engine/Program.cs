a7d47c920d6c1ea73547293045212e1e494e4bb2
Anindya Sarkar <kumarAnindyan@outlook.com>
2020-12-15 10:59:17 +0530

    GetTotalPrice Method with Individual Prices

--- Promotion_Engine/Program.cs
+++ Promotion_Engine/Program.cs
@@ -22,5 +22,42 @@ namespace Promotion_Engine
                 products.Add(p);
             }
         }
+        public static void GetTotalPrice(List<Product> products)
+        {
+            int counterofA = 0;
+            int priceofA = 50;
+            int counterofB = 0;
+            int priceofB = 30;
+            int CounterofC = 0;
+            int priceofC = 20;
+            int CounterofD = 0;
+            int priceofD = 15;
+
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A" || pr.Id == "a")
+                {
+                    counterofA = counterofA + 1;
+                }
+                if (pr.Id == "B" || pr.Id == "b")
+                {
+                    counterofB = counterofB + 1;
+                }
+                if (pr.Id == "C" || pr.Id == "c")
+                {
+                    CounterofC = CounterofC + 1;
+                }
+                if (pr.Id == "D" || pr.Id == "d")
+                {
+                    CounterofD = CounterofD + 1;
+                }
+            }
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+            Console.WriteLine(totalPriceofA+ totalPriceofB+ totalPriceofC+ totalPriceofD);
+
+        }
     }
 }
